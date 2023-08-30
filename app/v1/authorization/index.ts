8036553ba2a1bfbc9caa3227e0818c2c17e604c8
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-08-29 23:51:52 -0300

    .

--- app/v1/authorization/index.ts
+++ app/v1/authorization/index.ts
@@ -8,11 +8,22 @@ interface AuthorizationProps {
 }
 
 router.post("/", (req: Request, res: Response) => {
-  const { KeymasterId, hwid, script } = req.body as AuthorizationProps;
+  /* const { KeymasterId, hwid, script } = req.body as AuthorizationProps;
   const clientIPPerExpress =
     req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-  console.log(KeymasterId, hwid, script, clientIPPerExpress);
+  console.log(KeymasterId, hwid, script, clientIPPerExpress); */
+
+  res.send({ message: "Autenticado!", version: "1.0.0" });
+});
+
+router.post("/update", (req: Request, res: Response) => {
+  res.send({
+    message: "Autenticado!",
+    clients: [{ name: "client.lua", code: "print('client')" }],
+    servers: [{ name: "server.lua", code: "print('server')" }],
+    version: "1.0.0",
+  });
 });
 
 export { router as Authorization };
