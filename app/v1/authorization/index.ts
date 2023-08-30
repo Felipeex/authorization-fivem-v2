4a00d646175a7d622fa247e19af1a6310e2378a9
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-08-30 13:50:42 -0300

    .

--- app/v1/authorization/index.ts
+++ app/v1/authorization/index.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 const router = Router();
 
 interface AuthorizationProps {
@@ -7,23 +7,45 @@ interface AuthorizationProps {
   KeymasterId: string;
 }
 
-router.post("/", (req: Request, res: Response) => {
+async function handleAuthorization(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   /* const { KeymasterId, hwid, script } = req.body as AuthorizationProps;
   const clientIPPerExpress =
     req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
   console.log(KeymasterId, hwid, script, clientIPPerExpress); */
+  next();
+}
 
+router.post("/", handleAuthorization, (req: Request, res: Response) => {
   res.send({ message: "Autenticado!", version: "1.0.0" });
 });
 
-router.post("/update", (req: Request, res: Response) => {
+router.post("/install", handleAuthorization, (req: Request, res: Response) => {
+  const url = `${req.protocol}://${req.get("host")}${req.baseUrl}`;
   res.send({
     message: "Autenticado!",
     clients: [{ name: "client.lua", code: "print('client')" }],
-    servers: [{ name: "server.lua", code: "print('server')" }],
+    servers: [{ name: "server.lua", code: defaultCodeServer(url) }],
     version: "1.0.0",
   });
 });
+
+function defaultCodeServer(url: string, code?: string) {
+  return `CreateThread(function()
+  if not (Authorization.isReWritingFunction()) then
+      Authorization.getIp(function(ip)
+        PerformHttpRequest("${url}", function(err, data)
+          Authorization.handler(err, json.decode(data))
+        end, Authorization.Send())
+      end)
+    else
+      Authorization.print("CUIDADO", "Você não pode alterar nosso código.")
+    end
+  end)`;
+}
 
 export { router as Authorization };
