b56e37238c7e50a08f12407696a8f831e355377d
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-10 13:38:05 -0300

    remove dist

--- dist/v1/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.V1 = exports.router = void 0;
-const express_1 = require("express");
-const authorization_1 = require("./authorization");
-exports.router = (0, express_1.Router)();
-exports.V1 = exports.router;
-exports.router.use("/authorization", authorization_1.Authorization);
