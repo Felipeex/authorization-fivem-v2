916e4222a1a386d6874ae951127fff43cba289ca
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-15 10:15:12 -0300

    feat: insert cors.

--- app/index.ts
+++ app/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { V1 } from "./v1";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/v1", V1);
 app.listen(5555, () => {
