c77cc95bbefe80982bee05b3b79fdcd1d68aa9af
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-18 19:31:16 -0300

    IP

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+//@ts-ignore
+import expressPublicIp from "express-public-ip";
 import { V1 } from "./v1";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.set("trust proxy", true);
+app.enable("trust proxy");
+app.use(expressPublicIp());
 
 app.use("/v1", V1);
 app.listen(5555, () => {
