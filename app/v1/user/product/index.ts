aca03b506697192ea547cc133a8fcfce2a1a8b19
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-09-17 22:12:53 -0300

    remove test

--- app/v1/user/product/index.ts
+++ app/v1/user/product/index.ts
@@ -103,7 +103,7 @@ router.post(
       where: { ownerId: userExist.id, name: body.name },
     });
     if (productExist?.name === body.name)
-      return res.status(400).send({ error: "product name exist!!!" });
+      return res.status(400).send({ error: "product name exist" });
 
     try {
       await prisma.product.create({
