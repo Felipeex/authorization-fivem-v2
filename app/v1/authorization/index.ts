a0ed035ad41c1bc52c1b0109edb052f82597aaab
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-11-27 16:13:36 -0300

    fix: remove console

--- app/v1/authorization/index.ts
+++ app/v1/authorization/index.ts
@@ -21,10 +21,6 @@ async function handleAuthorization(
   const { KeymasterId, hwid, script } = req.body as AuthorizationProps;
   const clientIPPerExpress = req.ip;
 
-  console.log(clientIPPerExpress)
-
-  console.log(req)
-
   if (!KeymasterId || !hwid || !script)
     return res.send({
       message: "^1ERROR: Todos campos precisa ser preenchido.",
